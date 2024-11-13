138edf20c22a983a7a1677601b52313ceb3c4aaa
Premkumar <premkumar.zangroups@gmail.com>
2024-11-13 18:02:33 +0530

    Lesson5

---
+++ 5G.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+
+  </head>
+<body>
+<script>
+   
+    const cost = 5 + 2*3 +9;
+    console.log(`cost of food : $${cost}`);
+
+    const tax = cost *0.1;
+    console.log(`tax(10%):$${tax}`);
+
+    const totalcost = cost + tax ;
+
+
+  </script>  
+
+</body>
+
+
+
+
+
+</html>
+
